bbb909b2ae70749468c4de739bebffe928312346
Kenton Hamaluik <kenton@hamaluik.ca>
2019-01-14 07:35:35 -0700

    added compose key mod

--- install.sh
+++ install.sh
@@ -10,4 +10,7 @@ echo "ZSH - SPACESHIP"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
+echo "COMPOSE KEY AS MENU"
+setxkbmap -option compose:menu
+
 echo "OK, DO THE REST MANUALLY UNTIL YOU GET LESS LAZY"
